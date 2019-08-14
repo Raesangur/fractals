cee9e534a579dfd6b3f13cdf1de829f7500e71ab
Pascal-Emmanuel Lachance <1639571@cegeplimoilou.ca>
2019-08-14 19:58:51 -0400

    THEY DON'T WORK

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fractale
+{
+    public partial class Form1 : Form
+    {
+        List<triangle> tri = new List<triangle>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Brush B = new SolidBrush(Color.White);
+            Pen P = new Pen(B);
+            Graphics G = this.CreateGraphics();
+
+            triangle t = new triangle(G, P);
+            tri.Add(t);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(triangle t in tri)
+            {
+                t.draw();
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            int count = tri.Count;
+            for (int i = 0; i < count; i++)
+            {
+                triangle t1 = new triangle(tri[i], 0);
+                triangle t2 = new triangle(tri[i], 1);
+                triangle t3 = new triangle(tri[i], 2);
+                tri.Add(t1);
+                tri.Add(t2);
+                tri.Add(t3);
+            }
+            //this.Update();
+            this.Refresh();
+        }
+    }
+}
---
+++ triangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace fractale
+{
+    class triangle
+    {
+        public Graphics G;
+        public Pen P;
+        public Point haut;
+        public Point gauche;
+        public Point droite;
+        public bool drawn = false;
+
+        public triangle(Graphics g, Pen p)
+        {
+            G = g;
+            P = p;
+            gauche = new Point(0, 0);
+            droite = new Point(200, 0);
+            haut = new Point(100, 200);
+        }
+        public triangle(triangle t, int section)
+        {
+            G = t.G;
+            P = t.P;
+            if (section == 0)
+            {
+                gauche = new Point(t.gauche.X, t.gauche.Y);
+                droite = new Point((t.droite.X - t.gauche.X) / 2, t.droite.Y);
+                haut = new Point((t.droite.X - t.gauche.X) / 4, (t.droite.Y - t.gauche.Y) / 2);
+            }
+            else if(section == 1)
+            {
+                gauche = new Point((t.droite.X - t.gauche.X) / 2, t.droite.Y);
+                droite = new Point(t.droite.X, t.droite.Y);
+                haut = new Point(((t.droite.X - t.gauche.X) / 2) + droite.X, (t.droite.Y - t.gauche.Y) / 2);
+            }
+            else
+            {
+                gauche = new Point((t.droite.X - t.gauche.X) / 4, (t.droite.Y - t.gauche.Y) / 2);
+                droite = new Point(((t.droite.X - t.gauche.X) / 2) + droite.X, (t.droite.Y - t.gauche.Y) / 2);
+                haut = new Point(t.haut.X, t.haut.Y);
+            }
+        }
+
+        public void draw()
+        {
+            if (drawn == false)
+            {
+                G.DrawLine(P, gauche, droite);
+                G.DrawLine(P, droite, haut);
+                G.DrawLine(P, haut, gauche);
+                drawn = true;
+            }
+        }
+    }
+}
